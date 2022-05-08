0b1e4fc839e3b9b0bdb91fc6ccf578dbd2399189
Derek J. Clark <derekjohn.clark@gmail.com>
2022-05-08 09:46:04 -0700

    Updated file permissions

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Installing Media Support Package"
 cp -Rv media-support /usr/lib/
 cp -v 99-media-mount.rules /etc/udev/rules.d/
 cp -v "media-mount@.service" /etc/systemd/system/
-udevadm control --reoad-rules
+udevadm control -R
 systemctl daemon-reload
 
 exit 0
--- remove.sh
+++ remove.sh
@@ -5,7 +5,7 @@ echo "Removing Media Support Package"
 rm -rfv /usr/lib/media-support 
 rm -v /etc/udev/rules.d/99-media-mount.rules
 rm -v "/etc/systemd/system/media-mount@.service" 
-udevadm control --reoad-rules
+udevadm control -R
 systemctl daemon-reload
 
 exit 0
