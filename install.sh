fc12d2ebff2493e836919a1786ea97a57309feed
Derek J. Clark <derekjohn.clark@gmail.com>
2022-12-14 10:21:57 -0800

    Remove rules.d

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ cp -v usr/lib/systemd/system/media-mount@.service /usr/lib/systemd/system
 cp -v usr/lib/udev/rules.d/10-media-mount.rules /usr/lib/udev/rules.d/
 cp -v usr/lib/udev/rules.d/99-media-mount.rules /usr/lib/udev/rules.d/
 cp -v usr/share/polkit-1/actions/org.shadowblip.media-support.policy /usr/share/polkit-1/actions
-cp -v usr/share/polkit-1/rules.d/org.shadowblip.media-support.rules /usr/share/polkit-1/rules.d
 
 udevadm control -R
 systemctl daemon-reload
--- remove.sh
+++ remove.sh
@@ -8,8 +8,7 @@ rm -rfv /usr/lib/media-support
 rm -v /usr/lib/systemd/system/media-mount@.service
 rm -v /usr/lib/udev/rules.d/10-media-mount.rules
 rm -v /usr/lib/udev/rules.d/99-media-mount.rules
-rm -v /usr/share/polkit-1/actions/org.shadowblip.media-suppurt.policy
-rm -v /usr/share/polkit-1/rules.d/org.shadowblip.media-support.rules
+rm -v /usr/share/polkit-1/actions/org.shadowblip.media-support.policy
 
 udevadm control -R
 systemctl daemon-reload
