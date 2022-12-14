ed95a4d51cdb8ad24bb2334307a09e1a4d275375
Derek J. Clark <derekjohn.clark@gmail.com>
2022-12-14 09:58:16 -0800

    Fix install/remove. Update README.md. Add rules.

--- remove.sh
+++ remove.sh
@@ -8,7 +8,8 @@ rm -rfv /usr/lib/media-support
 rm -v /usr/lib/systemd/system/media-mount@.service
 rm -v /usr/lib/udev/rules.d/10-media-mount.rules
 rm -v /usr/lib/udev/rules.d/99-media-mount.rules
-rm -v /share/polkit-1/actions/org.shadowblip.media-suppurt.policy
+rm -v /usr/share/polkit-1/actions/org.shadowblip.media-suppurt.policy
+rm -v /usr/share/polkit-1/rules.d/org.shadowblip.media-support.rules
 
 udevadm control -R
 systemctl daemon-reload
